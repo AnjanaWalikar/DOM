73590e19d2a6d459ae599b372ede3e957b9a6355
Anjana R <anjanawalikar155@gmail.com>
2025-02-13 15:36:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 function showAlert(){
-    alert('Hello! the button is clicked, this is JS class')
+    alert('Hello! This is a Javascript alert.');
 }
 function changeText(){
-    document.getElementById('text').innerHTML="Text changed";
+    document.getElementById('text').innerHTML = 'Text changed!';
+}
+function handleClick(){
+    document.getElementById('clickMessage').innerHTML = 'Button clicked!';
+}
+function validateForm(){
+    let name = document.getElementById("name").value;
+    if(name ==" ") {
+        alert('Please enter your name.');
+        return false;
+    }
+    return true;
+}
+function checkNumber(){
+    let num = parseInt(document.getElementById('numberInput').value);
+    let result = num > 10 ? 'Greater than 10' : '10 or less';
+    document.getElementById('numberResult').innerHTML = result;
+}
+function checkAge(){
+    let age = parseInt(document.getElementById('ageInput').value);
+    if(age >= 18){
+        document.getElementById('ageResult').innerHTML = 'You are an adult';
+    }else{
+        document.getElementById('ageResult').innerHTML = 'You are minor';
+    }
+}
+function displayArrayItem(){
+    let items = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+    let index = parseInt(document.getElementById('arrayIndex').value);
+    let result = items[index] || 'Invalid index';
+    document.getElementById('arrayResult').innerHTML = result;
 }
